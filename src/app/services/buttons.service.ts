bf3b954eb734a569cf47ce01edc4619ccb2abc6f
Pawel <plpasza@gmail.com>
2021-04-20 12:30:30 +0200

    added button interface with children

--- src/app/services/buttons.service.ts
+++ src/app/services/buttons.service.ts
@@ -1,81 +1,92 @@
-import { Injectable } from '@angular/core';
+import { Injectable, InjectionToken } from '@angular/core';
 import { SystemsServiceService } from './systems-service.service';
 import {_} from 'lodash';
 
+interface Button {
+  text: string;
+  link: string;
+  bg: string;
+  children?: Array<Button>;
+}
+
+
+const navButtons: Array<Button> = [
+  {
+    text: "About",
+    link:'/about',
+    bg:'none'
+  },
+  // {
+  //   text: "Login",
+  //   link: "/login",
+  //   bg:'none'
+  // },
+  
+]
+
+const commonNavButtons: Array<Button> = [
+  {
+    text: "Home",
+    link: "",
+    bg:'none'
+  },
+  {
+    text: "Players",
+    link:'players',
+    bg:'none'
+  },
+  {
+    text: "Characters",
+    link: "characters",
+    bg:'none'
+  },
+  {
+    text: "Monsters",
+    link:'monsters',
+    bg:'none'
+  },
+]
+
+const wfrpNavButtons:  Array<Button> = [
+  {
+    text: "Manage",
+    link: "/mange",
+    bg: "none",
+    children: [
+      {
+        text: "Abilities",
+        link: "abilities",
+        bg:'none'
+      },
+      {
+        text: "Spells",
+        link: "/WFRP/spells",
+        bg:"none"
+      }
+    ]
+  }
+]
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ButtonsService {
-
-  navButtons: Array<{text: String, link: String, bg:String}> = [
-    {
-      text: "About",
-      link:'/about',
-      bg:'none'
-    },
-    // {
-    //   text: "Login",
-    //   link: "/login",
-    //   bg:'none'
-    // },
-    
-  ]
-
-  commonNavButtons: Array<{text: String, link: String, bg:String}> = [
-    {
-      text: "Home",
-      link: "",
-      bg:'none'
-    },
-    {
-      text: "Players",
-      link:'players',
-      bg:'none'
-    },
-    {
-      text: "Characters",
-      link: "characters",
-      bg:'none'
-    },
-    {
-      text: "Monsters",
-      link:'monsters',
-      bg:'none'
-    },
-    {
-      text: "Abilities",
-      link: "abilities",
-      bg:'none'
-    },
-  ]
-
-  wfrpNavButtons:  Array<{text: String, link: String}> = [
-    {
-      text: "Spells",
-      link: "/WFRP/spells"
-    }
-  ]
-
-  constructor(private systemService: SystemsServiceService) { }
 
   getNavButtons(system: String){
     let buttons = [];
     if(system){
       
-      for(let i = 0; i < this.commonNavButtons.length; i++){
+      for(let i = 0; i < commonNavButtons.length; i++){
         buttons.push({
-          text:this.commonNavButtons[i].text,
-          link: system + "/" + this.commonNavButtons[i].link,
+          text:commonNavButtons[i].text,
+          link: system + "/" + commonNavButtons[i].link,
           bg:'none'
         })
       }
       if(system === 'WFRP'){
-        buttons = buttons.concat(this.wfrpNavButtons)
+        buttons = buttons.concat(wfrpNavButtons)
       }
     }
-
-    
-    
-    return buttons.concat(this.navButtons);
+    return buttons.concat(navButtons);
   }
 }
